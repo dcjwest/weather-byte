b80bca10c61f114ec764d7c2df28e1fee2547d9b
David van der Westhuizen <dcjwest@gmail.com>
2020-05-17 18:21:44 +0200

    Updates

---
+++ precache-manifest.bd1e8280ee42bce663a714c90e403ff2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4929b214c2365862ad07893afe4d6968",
+    "url": "/weather-byte/index.html"
+  },
+  {
+    "revision": "9d9d941fdce8db6c7b15",
+    "url": "/weather-byte/static/css/main.c214dd87.chunk.css"
+  },
+  {
+    "revision": "805dbe0db01e522c2f0d",
+    "url": "/weather-byte/static/js/2.ed7d140b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather-byte/static/js/2.ed7d140b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9d941fdce8db6c7b15",
+    "url": "/weather-byte/static/js/main.e9823861.chunk.js"
+  },
+  {
+    "revision": "7e2a2e93c20f1ab386be",
+    "url": "/weather-byte/static/js/runtime-main.6a6d7b65.js"
+  },
+  {
+    "revision": "fbbe405ec9ef5e96f231c3c6ae8a7b81",
+    "url": "/weather-byte/static/media/clear-day.fbbe405e.svg"
+  },
+  {
+    "revision": "08655708f41966cf1833dc094c334ca2",
+    "url": "/weather-byte/static/media/clear-night.08655708.svg"
+  },
+  {
+    "revision": "4de8985cfa35dcd289df5cbed8a218e7",
+    "url": "/weather-byte/static/media/cloud-bite.4de8985c.svg"
+  },
+  {
+    "revision": "fc330852107182fd37506445fd52930d",
+    "url": "/weather-byte/static/media/cloud.fc330852.svg"
+  },
+  {
+    "revision": "eada737d88513942ae702146eae62aa3",
+    "url": "/weather-byte/static/media/cloud_bg1.eada737d.png"
+  },
+  {
+    "revision": "fdc1c2a62c8aed4e8aa3d289e6978869",
+    "url": "/weather-byte/static/media/cloud_bg2.fdc1c2a6.png"
+  },
+  {
+    "revision": "406914f61e7a57feae5f01408f1d6c0c",
+    "url": "/weather-byte/static/media/cloud_bg3.406914f6.png"
+  },
+  {
+    "revision": "070fbd13dd1d99f7c95df5da1ca64160",
+    "url": "/weather-byte/static/media/cloud_bg4.070fbd13.png"
+  },
+  {
+    "revision": "0db8e72879f7d411bdf5c5bd9e205ca4",
+    "url": "/weather-byte/static/media/cloud_bg5.0db8e728.png"
+  },
+  {
+    "revision": "bdfd90660e255931a9574cc6c4371fbd",
+    "url": "/weather-byte/static/media/cloudy.bdfd9066.svg"
+  },
+  {
+    "revision": "d34217fb4d19e9579f64f9456d7fd6a2",
+    "url": "/weather-byte/static/media/fog.d34217fb.svg"
+  },
+  {
+    "revision": "8b4ad53c664cceea8dedab4c8c1984f8",
+    "url": "/weather-byte/static/media/partly-cloudy-day.8b4ad53c.svg"
+  },
+  {
+    "revision": "b63ded907e0a6ad3a8429a9145cd66de",
+    "url": "/weather-byte/static/media/partly-cloudy-night.b63ded90.svg"
+  },
+  {
+    "revision": "5fcdec4dadf12c39bfdf409a2e8391e3",
+    "url": "/weather-byte/static/media/rain.5fcdec4d.svg"
+  },
+  {
+    "revision": "a1cee43e70eb3d8d11207d262dec3b2d",
+    "url": "/weather-byte/static/media/snow.a1cee43e.svg"
+  },
+  {
+    "revision": "381339ecf9ca80876e4d0756ce80462d",
+    "url": "/weather-byte/static/media/thunderstorm.381339ec.svg"
+  },
+  {
+    "revision": "77fc2379feb2ab670a641469b21dcc09",
+    "url": "/weather-byte/static/media/wind.77fc2379.svg"
+  }
+]);
