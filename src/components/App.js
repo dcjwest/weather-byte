02813fe569493e9184ea4ab96ae8dcf87b1fa359
David van der Westhuizen <dcjwest@gmail.com>
2020-08-25 21:50:46 +0200

    Added rain percentage indicator for hourly forecast. Only visible for >= 50%.

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
-import { dummyData } from '../context/dummyData';
+import React, { useState, useEffect, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { dummyData } from "../context/dummyData";
 
 // Components
-import Container from 'react-bootstrap/Container';
-import Loader from './Loader/Loader';
-import Options from './Options/Options';
-import Summary from './Summary/Summary';
-import HourlyForecast from './HourlyForecast';
-import DailyForecast from './DailyForecast';
-import ComfortLevel from './ComfortLevel/ComfortLevel';
-import Wind from './Wind/Wind';
-import SunAndMoon from './SunAndMoon/SunAndMoon';
-import Footer from './Footer';
-import Error from './Error/Error';
-import iconList from '../assets/iconIndex';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Loader from "./Loader/Loader";
+import Options from "./Options/Options";
+import Summary from "./Summary/Summary";
+import HourlyForecast from "./HourlyForecast";
+import DailyForecast from "./DailyForecast";
+import ComfortLevel from "./ComfortLevel/ComfortLevel";
+import Wind from "./Wind/Wind";
+import SunAndMoon from "./SunAndMoon/SunAndMoon";
+import Footer from "./Footer";
+import Error from "./Error/Error";
+import iconList from "../assets/iconIndex";
+import "./App.css";
 
 const App = () => {
   const [appLoaded, setAppLoaded] = useState(false);
   const [showError, setShowError] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState('');
-  const [weatherApi, setWeatherApi] = useState('');
+  const [currentLocation, setCurrentLocation] = useState("");
+  const [weatherApi, setWeatherApi] = useState("");
   const [weatherData, setWeatherData] = useState({});
-  const [updateTime, setUpdateTime] = useState('');
+  const [updateTime, setUpdateTime] = useState("");
   const { setInitialWeather, daily } = useContext(GlobalContext);
   let sunrise_time, sunset_time;
 
@@ -33,12 +33,15 @@ const App = () => {
     // Create Promise to handle user's response before making API call.
     return new Promise((resolve, reject) => {
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-          userLatitude = position.coords.latitude;
-          userLongitude = position.coords.longitude;
-          resolve({ userLatitude, userLongitude });
-          // Note: getCurrentPosition takes an error-handling callback as 2nd argument, therefore apply REJECT here.
-        }, err => reject(err));
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            userLatitude = position.coords.latitude;
+            userLongitude = position.coords.longitude;
+            resolve({ userLatitude, userLongitude });
+            // Note: getCurrentPosition takes an error-handling callback as 2nd argument, therefore apply REJECT here.
+          },
+          (err) => reject(err)
+        );
       }
     });
   }
@@ -46,8 +49,9 @@ const App = () => {
   // Code below run only on-mount to determine user's location and fetch weather data.
   useEffect(() => {
     async function getInitialWeatherData() {
-      const DEFAULT_LATITUDE = -33.934444, DEFAULT_LONGITUDE = 18.869167;  // Stellenbosch set as default.
-      const proxy = 'https://cors-anywhere.herokuapp.com/'; // Enables cross-origin requests to anywhere.
+      const DEFAULT_LATITUDE = -33.934444,
+        DEFAULT_LONGITUDE = 18.869167; // Stellenbosch set as default.
+      const proxy = "https://cors-anywhere.herokuapp.com/"; // Enables cross-origin requests to anywhere.
       let lat, long;
 
       // Set user's coordinates if provided.
@@ -55,39 +59,44 @@ const App = () => {
         let userResponse = await getUserPosition();
         lat = userResponse.userLatitude;
         long = userResponse.userLongitude;
-      }
-      // Use default coordinates if geolocation request is denied.
-      catch (err) {
+      } catch (err) {
+        // Use default coordinates if geolocation request is denied.
         lat = DEFAULT_LATITUDE;
         long = DEFAULT_LONGITUDE;
       }
 
       // Get location with given coordinates.
-      fetch(`https://us1.locationiq.com/v1/reverse.php?key=56552b4b1c4b9a&lat=${lat}&lon=${long}&format=json`)
-        .then(res => res.json())
-        .then(data => setCurrentLocation(data.address.town))
-        .catch(err => {
-          console.log('LocationIQ: There was a problem... ', err);
-          if (lat === DEFAULT_LATITUDE && long === DEFAULT_LONGITUDE) setCurrentLocation('Stellenbosch');
-          else setCurrentLocation('Unknown');
+      fetch(
+        `https://us1.locationiq.com/v1/reverse.php?key=56552b4b1c4b9a&lat=${lat}&lon=${long}&format=json`
+      )
+        .then((res) => res.json())
+        .then((data) => setCurrentLocation(data.address.town))
+        .catch((err) => {
+          console.log("LocationIQ: There was a problem... ", err);
+          if (lat === DEFAULT_LATITUDE && long === DEFAULT_LONGITUDE)
+            setCurrentLocation("Stellenbosch");
+          else setCurrentLocation("Unknown");
         });
 
       // Retrieve weather data for particular location. SI units used by default.
-      fetch(`${proxy}https://api.darksky.net/forecast/6385bf526a557ab35c6534562b693fdc/${lat},${long}?units=si`)
-        .then(res => res.json())
-        .then(data => setWeatherData(data))
-        .catch(err => {
-          console.log('DarkSky: There was a problem... ', err);
+      fetch(
+        `${proxy}https://api.darksky.net/forecast/6385bf526a557ab35c6534562b693fdc/${lat},${long}?units=si`
+      )
+        .then((res) => res.json())
+        .then((data) => setWeatherData(data))
+        .catch((err) => {
+          console.log("DarkSky: There was a problem... ", err);
           setShowError(true);
           setInitialWeather(dummyData);
         });
 
       // Store API string to be able to make future update requests.
-      setWeatherApi(`${proxy}https://api.darksky.net/forecast/6385bf526a557ab35c6534562b693fdc/${lat},${long}`);
+      setWeatherApi(
+        `${proxy}https://api.darksky.net/forecast/6385bf526a557ab35c6534562b693fdc/${lat},${long}`
+      );
     }
 
     getInitialWeatherData();
-
   }, [setInitialWeather]);
 
   // Initialise app with retrieved data.
@@ -101,38 +110,57 @@ const App = () => {
 
   // Set the time of the last API request.
   function setLastUpdateTime() {
-      const now = new Date();
-      const hours = now.getHours();
-      const minutes = now.getMinutes();
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
 
-      setUpdateTime(`Last updated: ${hours < 10? '0'+hours:hours}:${minutes < 10? '0'+minutes:minutes}`);
+    setUpdateTime(
+      `Last updated: ${hours < 10 ? "0" + hours : hours}:${
+        minutes < 10 ? "0" + minutes : minutes
+      }`
+    );
   }
 
   /* Convert API timestamp (given in seconds) to readable date-time values. */
   function convertUnixTime(timeStamp) {
-    const nextDate = new Date(timeStamp * 1000) // Date object uses milliseconds.
-    const dayArr = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
+    const nextDate = new Date(timeStamp * 1000); // Date object uses milliseconds.
+    const dayArr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     const monthArr = [
-      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
     ];
     const nextHour = nextDate.getHours();
-    
+
     return {
-      hour: `${nextHour < 10? '0'+nextHour : nextHour}:00`,
+      hour: `${nextHour < 10 ? "0" + nextHour : nextHour}:00`,
       date: nextDate.getDate(),
       day: dayArr[nextDate.getDay()],
-      month: monthArr[nextDate.getMonth()]
-    }
+      month: monthArr[nextDate.getMonth()],
+    };
   }
 
   function getWeatherIcon(weatherType) {
-    return iconList[weatherType.replace(/-/g, '_')];
+    return iconList[weatherType.replace(/-/g, "_")];
   }
 
   // Set the background gradient based on time of day.
   function setTheme() {
-    const dayTheme = window.getComputedStyle(document.documentElement).getPropertyValue('--bg-day');
-    const nightTheme = window.getComputedStyle(document.documentElement).getPropertyValue('--bg-night');
+    const dayTheme = window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue("--bg-day");
+    const nightTheme = window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue("--bg-night");
     const timeNow = new Date().getTime() / 1000;
 
     if (Object.keys(daily).length !== 0) {
@@ -140,7 +168,9 @@ const App = () => {
       sunrise_time = sunriseTime;
       sunset_time = sunsetTime;
 
-      return timeNow < sunrise_time || timeNow > sunset_time? nightTheme : dayTheme;
+      return timeNow < sunrise_time || timeNow > sunset_time
+        ? nightTheme
+        : dayTheme;
     }
 
     return dayTheme;
@@ -152,23 +182,38 @@ const App = () => {
 
   return (
     <>
-      <div className='background' style={{backgroundImage: setTheme()}}></div>
+      <div className="background" style={{ backgroundImage: setTheme() }}></div>
       <Loader appLoaded={appLoaded} showError={showError} />
-      { showError && <Error hideError={() => hideError()}/>}
-      <div className='App'>
+      {showError && <Error hideError={() => hideError()} />}
+      <div className="App">
         <Container>
-          <Options setLastUpdateTime={setLastUpdateTime} weatherApi={weatherApi} />
-          <Summary currentLocation={currentLocation} updateTime={updateTime} /><hr />
-          <HourlyForecast convertUnixTime={convertUnixTime} getWeatherIcon={getWeatherIcon} /><hr />
-          <DailyForecast convertUnixTime={convertUnixTime} getWeatherIcon={getWeatherIcon} /><hr />
-          <ComfortLevel /><hr />
-          <Wind /><hr />
-          <SunAndMoon /><hr />
+          <Options
+            setLastUpdateTime={setLastUpdateTime}
+            weatherApi={weatherApi}
+          />
+          <Summary currentLocation={currentLocation} updateTime={updateTime} />
+          <hr />
+          <HourlyForecast
+            convertUnixTime={convertUnixTime}
+            getWeatherIcon={getWeatherIcon}
+          />
+          <hr />
+          <DailyForecast
+            convertUnixTime={convertUnixTime}
+            getWeatherIcon={getWeatherIcon}
+          />
+          <hr />
+          <ComfortLevel />
+          <hr />
+          <Wind />
+          <hr />
+          <SunAndMoon />
+          <hr />
           <Footer />
         </Container>
       </div>
     </>
   );
-}
+};
 
 export default App;
--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,58 +1,76 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
-import Swiper from 'react-id-swiper';
-import { IconContext } from 'react-icons';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Swiper from "react-id-swiper";
+import { IconContext } from "react-icons";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const HourlyForecast = ({ convertUnixTime, getWeatherIcon }) => {
-    // Swiper Parameters
-    const params = {
-        slidesPerView: 6,
-        spaceBetween: 5,
-        freeMode: true,
-        grabCursor: true
-    }
-    const { hourly } = useContext(GlobalContext);
-    // Initialise hourCarousel with placeholder markup to ensure Swiper configures correctly.
-    let hourCarousel = [...Array(24).keys()].map(x => ++x).map((item, index) => {
-        return (
-            <div key={index} className='hourly-slide flex-center'>
-                <span className='swiper-placeholder'>hour {item}</span>
-                <span className='swiper-placeholder'>icon {item}</span>
-                <span className='swiper-placeholder'>temp {item}</span>
-            </div>
-        );
+  // Swiper Parameters
+  const params = {
+    slidesPerView: 6,
+    spaceBetween: 5,
+    freeMode: true,
+    grabCursor: true,
+  };
+  const { hourly } = useContext(GlobalContext);
+  // Initialise hourCarousel with placeholder markup to ensure Swiper configures correctly.
+  let hourCarousel = [...Array(24).keys()]
+    .map((x) => ++x)
+    .map((item, index) => {
+      return (
+        <div key={index} className="hourly-slide flex-center">
+          <span className="swiper-placeholder">hour {item}</span>
+          <span className="swiper-placeholder">icon {item}</span>
+          <span className="swiper-placeholder">temp {item}</span>
+        </div>
+      );
     });
 
-    if (Object.keys(hourly).length !== 0) {
-        hourCarousel = hourly.map((hourItem, index) => {
-            let currentHour = new Date(hourItem.time*1000).getHours();
-            let icon = hourItem.icon;
-            // API doesn't differentiate between cloudy day/night, so it is checked here.
-            if (icon === 'cloudy') {
-                if (currentHour >= 18 || currentHour <= 7) icon = 'cloudy-night';
-                else icon = 'cloudy-day';
-            }
+  if (Object.keys(hourly).length !== 0) {
+    hourCarousel = hourly.map((hourItem, index) => {
+      let currentHour = new Date(hourItem.time * 1000).getHours();
+      let icon = hourItem.icon;
+      // API doesn't differentiate between cloudy day/night, so it is checked here.
+      if (icon === "cloudy") {
+        if (currentHour >= 18 || currentHour <= 7) icon = "cloudy-night";
+        else icon = "cloudy-day";
+      }
+      // Determine the percentage chance of rain.
+      let rainPercentage = hourItem.precipProbability * 100;
 
-            return (
-                <div key={index} className='hourly-slide flex-center'>
-                    <span className='slide-time'>{convertUnixTime(hourItem.time).hour}</span>
-                    <img className='slide-icon' src={getWeatherIcon(icon)} alt={hourItem.summary} title={hourItem.summary} />
-                    <span className='slide-temp'>{Math.round(hourItem.temperature)}</span>
-                </div>
-            );
-        });
-    }
-
-    return (
-        <div className='hourly-forecast-swiper flex-center'>
-            <IconContext.Provider value={{className: 'swiper-icon'}}><IoIosArrowBack /></IconContext.Provider>
-            <Swiper {...params}>
-                { hourCarousel}
-            </Swiper>
-            <IconContext.Provider value={{className: 'swiper-icon'}}><IoIosArrowForward /></IconContext.Provider>
+      return (
+        <div key={index} className="hourly-slide flex-center">
+          <span className="slide-time">
+            {convertUnixTime(hourItem.time).hour}
+          </span>
+          <span
+            className={`slide-rain ${rainPercentage >= 50 ? "show" : "hide"}`}
+          >
+            {`${rainPercentage}%`}
+          </span>
+          <img
+            className="slide-icon"
+            src={getWeatherIcon(icon)}
+            alt={hourItem.summary}
+            title={hourItem.summary}
+          />
+          <span className="slide-temp">{Math.round(hourItem.temperature)}</span>
         </div>
-    );
-}
+      );
+    });
+  }
+
+  return (
+    <div className="hourly-forecast-swiper flex-center">
+      <IconContext.Provider value={{ className: "swiper-icon" }}>
+        <IoIosArrowBack />
+      </IconContext.Provider>
+      <Swiper {...params}>{hourCarousel}</Swiper>
+      <IconContext.Provider value={{ className: "swiper-icon" }}>
+        <IoIosArrowForward />
+      </IconContext.Provider>
+    </div>
+  );
+};
 
 export default HourlyForecast;
