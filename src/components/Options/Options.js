874b9c28b0beb4d3031e41bb06444198fbb7fe2b
David van der Westhuizen <dcjwest@gmail.com>
2020-09-08 20:46:46 +0200

    Fixed issue where toggling temperature unit in options without clicking update button switches units without converting actual values.

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -9,7 +9,7 @@ const Options = ({ setLastUpdateTime, weatherApi }) => {
     const { switchUnits, updateWeather } = useContext(GlobalContext);
     const [showOptions, setShowOptions] = useState(false);
     const [showUpdater, setShowUpdater] = useState(false);
-    const [useFahrenheit, setUseFahrenheit] = useState(false);
+    const [currentUnit, setCurrentUnit] = useState('cel');
 
     // Add an overlay when options button is clicked to handle clicking elsewhere or scrolling away.
     useEffect(() => {
@@ -19,77 +19,93 @@ const Options = ({ setLastUpdateTime, weatherApi }) => {
         return () => {
             overlay.removeEventListener('click', () => setShowOptions(false));
             window.removeEventListener('scroll', () => setShowOptions(false));
-        }
+        };
     }, []);
 
     // Update weather info by making new API request with either Celcius(SI) or Fahrenheit(Imperial) units.
     function handleUpdate() {
-        let nextRequest = weatherApi;
+        let nextRequest =
+            currentUnit === 'cel' ? `${weatherApi}?units=si` : weatherApi;
 
-        if (!useFahrenheit) {
-            nextRequest = `${weatherApi}?units=si`;
-        }
         // Hide Options dialogue and show Updating screen while waiting for API response.
         setShowUpdater(true);
         setShowOptions(false);
 
-        fetch(nextRequest).then(res => res.json()).then(data => {
-            updateWeather(data);
-            setLastUpdateTime();
-            setShowUpdater(false);
-        });
+        fetch(nextRequest)
+            .then(res => res.json())
+            .then(data => {
+                updateWeather(data);
+                switchUnits(currentUnit);
+                setLastUpdateTime();
+                setShowUpdater(false);
+            });
     }
 
     function handleRadioBtn(e) {
-        if (e.target.value === 'fah') {
-            setUseFahrenheit(true);
-            e.target.checked = true;
-
-        }
-        else {
-            setUseFahrenheit(false);
-            e.target.checked = true;
-        }
-        // Update state with unit setting.
-        switchUnits(e.target.value);
+        e.target.value === 'fah'
+            ? setCurrentUnit('fah')
+            : setCurrentUnit('cel');
     }
 
     return (
-        <div className='options-wrapper'>
-            <div className='options-btn-wrapper'>
-                <button className='options-btn' onClick={() => setShowOptions(!showOptions)}>
-                    <IconContext.Provider value={{className: 'options-icon'}} >
+        <div className="options-wrapper">
+            <div className="options-btn-wrapper">
+                <button
+                    className="options-btn"
+                    onClick={() => setShowOptions(!showOptions)}
+                >
+                    <IconContext.Provider value={{ className: 'options-icon' }}>
                         <BsThreeDotsVertical />
                     </IconContext.Provider>
                 </button>
-                <div className={`options-list ${showOptions? 'show':'hide'}`}>
-                    <form className='unit-toggle-wrapper' onSubmit={e => e.preventDefault()}>
-                        <label htmlFor='cel'>
-                            <input type='radio' id='cel' value='cel' name='unit' onChange={handleRadioBtn}/>&deg;C
+                <div
+                    className={`options-list ${showOptions ? 'show' : 'hide'}`}
+                >
+                    <form
+                        className="unit-toggle-wrapper"
+                        onSubmit={e => e.preventDefault()}
+                    >
+                        <label htmlFor="cel">
+                            <input
+                                type="radio"
+                                id="cel"
+                                value="cel"
+                                name="unit"
+                                checked={currentUnit === 'cel'}
+                                onChange={handleRadioBtn}
+                            />
+                            &deg;C
                         </label>
-                        <label htmlFor='fah'>
-                            <input type='radio' id='fah' value='fah' name='unit' onChange={handleRadioBtn}/>&deg;F
+                        <label htmlFor="fah">
+                            <input
+                                type="radio"
+                                id="fah"
+                                value="fah"
+                                name="unit"
+                                checked={currentUnit === 'fah'}
+                                onChange={handleRadioBtn}
+                            />
+                            &deg;F
                         </label>
                     </form>
                     <hr />
                     <button onClick={handleUpdate}>Update</button>
                 </div>
-                <div className={`overlay ${showOptions? 'show': ''}`}></div>
-                {
-                    showUpdater && 
-                    <div className='updating-screen flex-center'>
-                        <img src={cloudUrl} alt='cloud animation' />
+                <div className={`overlay ${showOptions ? 'show' : ''}`}></div>
+                {showUpdater && (
+                    <div className="updating-screen flex-center">
+                        <img src={cloudUrl} alt="cloud animation" />
                         <p>Updating</p>
-                        <div className='loading-dots flex-center'>
+                        <div className="loading-dots flex-center">
                             <div></div>
                             <div></div>
                             <div></div>
                         </div>
                     </div>
-                }
+                )}
             </div>
         </div>
     );
-}
+};
 
 export default Options;
